2eba839436fbe28b4af645515f578df941b16c85
Gabriel Grego <gabrielsgrego@uol.com.br>
2024-11-19 21:41:19 -0300

    tela de login finalizada

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { VStack, Input, Text, Image } from 'native-base';
-import { View, TextInput, Button} from 'react-native';
+import { VStack, Input, Text, Image, Button, Box } from 'native-base';
+import { ImageBackground, StyleSheet} from 'react-native';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { RootStackParamList } from '../navigation/types';
 
@@ -12,15 +12,31 @@ const LoginScreen: React.FC = () => {
     };
 
 
-return (
-    <VStack space={4} padding={5} alignItems="center">
-        <Text fontSize="2xl" bold> SOLINVESTE</Text>
-        <Text fontSize="xl" bold>Faça login com sua conta Enel e veja como investir no futuro.</Text>
-        <Input placeholder="Email" />
-        <Input placeholder="Senha" type="password" />
-        <Button title="Login" onPress={handleLogin}/>
-    </VStack>
+    return (
+        <ImageBackground
+            source={require('../components/bgLogin.jpg')}
+            style={styles.background}>
+        <VStack space={4} padding={5} alignItems="center" justifyContent="center" flex={1}>
+            <Image source={require('../components/logoApp.png')} alt="img" maxWidth={"100%"} />
+            <Box padding={2} bg={"white"} borderRadius={8}>
+                <Text fontSize="md" textAlign="center" color="black">
+                Faça login com sua conta Enel e veja como investir no futuro.
+                </Text>
+            </Box>            
+            <Input width={"85%"} placeholder="Email" backgroundColor="white" />
+            <Input width={"85%"} placeholder="Senha" type="password" backgroundColor="white" />
+            <Button my={4} onPress={handleLogin} w={"50%"} borderRadius={8} bg="orange.400" _text={{ bold: true, color: "white", fontSize:"md" }}>Login</Button>
+            <Image source={require('../components/logoSolari.png')} alt="img" size={'xs'} w={'100%'}/>
+        </VStack>
+        </ImageBackground>
     );
 };
-
+    
+    const styles = StyleSheet.create({
+        background: {
+        flex: 1,
+        resizeMode: 'cover'
+    }
+});
+    
 export default LoginScreen;

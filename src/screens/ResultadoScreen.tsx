dab7ff3098faa40259b82afa7446e7c6a9f1895b
Gabriel Grego <gabrielsgrego@uol.com.br>
2024-11-21 23:06:27 -0300

    ajustes de layout

--- src/screens/ResultadoScreen.tsx
+++ src/screens/ResultadoScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { VStack, Box, Text, Button, ScrollView } from 'native-base';
+import { HStack, VStack, Box, Text, Button, ScrollView } from 'native-base';
 import { NavigationProp, useNavigation, RouteProp, useRoute } from '@react-navigation/native';
 import { RootStackParamList } from '../navigation/types';
 import InfoCard from '../components/InfoCard';
@@ -33,58 +33,57 @@ return (
     <ScrollView flex={1} contentContainerStyle={{padding: 6}} bg="#FFBC59">
     <VStack space={4} >
         <InfoCard
-        title="Sistema indicado"
-        items={[
-            { label: 'Número de módulos', value: sistemaIndicado },
-            { label: 'Potência do sistema', value: potenciaSistema },
-            { label: 'Produção de energia estimada (média anual)', value: producaoEstimativa },
-        ]}
-        footer="Cada raio de sol é uma oportunidade de economizar e cuidar do planeta!"
+            title="Sistema indicado"
+            items={[
+                { label: 'Número de módulos', value: sistemaIndicado },
+                { label: 'Potência do sistema', value: potenciaSistema },
+                { label: 'Produção de energia estimada (média anual)', value: producaoEstimativa },
+            ]}
+            footer="Cada raio de sol é uma oportunidade de economizar e cuidar do planeta!"
         />
 
         <InfoCard
-        title="Investimento"
-        items={[
-            { label: 'Estimativa de investimento', value: estimativaInvestimento },
-            { label: 'Economia mensal', value: economiaMensal },
-            { label: 'Total economizado em 20 anos', value: totalEconomia },
-        ]}
-        footer="Energia solar é a escolha certa para seu bolso e o planeta!"
+            title="Investimento"
+            items={[
+                { label: 'Estimativa de investimento', value: estimativaInvestimento },
+                { label: 'Economia mensal', value: economiaMensal },
+                { label: 'Total economizado em 20 anos', value: totalEconomia },
+            ]}
+            footer="Energia solar é a escolha certa para seu bolso e o planeta!"
         />
 
         <InfoCard
-        title="Retorno do Investimento (under development)"
-        items={[
-            { label: '', value: '' },
-        ]}
-        footer=""
+            title="Retorno do Investimento (under development)"
+            items={[
+                { label: '', value: '' },
+            ]}
+            footer=""
         />
 
-    <InfoCard2
-        title="Retorno Ambiental (estimado em 20 anos)"
-        items={[
-        {
-            icon: 'cloud',
-            value: '41,32 toneladas',
-            description: 'de CO2 que não serão emitidos na atmosfera',
-        },
-        {
-            icon: 'nature',
-            value: '315 árvores',
-            description: 'seriam necessárias para eliminar a quantidade de CO2 da atmosfera',
-        },
-        {
-            icon: 'attach-money',
-            value: 'R$ 6.460,00',
-            description: 'é o custo aproximado para plantar essa quantidade de árvores',
-        },
-        ]}
-    />
-
-        <VStack space={4} mt={4}>
-            <Button colorScheme="orange" onPress={handleHome}>Aprovar proposta</Button>
-            <Button variant="outline" colorScheme="orange" onPress={handleHome}>Voltar ao início</Button>
-        </VStack>
+        <InfoCard2
+            title="Retorno Ambiental (estimado em 20 anos)"
+            items={[
+            {
+                icon: 'cloud',
+                value: '41,32 toneladas',
+                description: 'de CO2 que não serão emitidos na atmosfera',
+            },
+            {
+                icon: 'nature',
+                value: '315 árvores',
+                description: 'seriam necessárias para eliminar a quantidade de CO2 da atmosfera',
+            },
+            {
+                icon: 'attach-money',
+                value: 'R$ 6.460,00',
+                description: 'é o custo aproximado para plantar essa quantidade de árvores',
+            },
+            ]}
+        />
+        <HStack space={2} justifyContent={"space-between"} px={2} >
+            <Button variant="outline" onPress={handleHome} w={"50%"} borderRadius={8} bg="orange.400" _text={{ bold: true, color: "white", fontSize:"lg", textAlign: "center"}}>Aprovar proposta</Button>
+            <Button variant="outline" onPress={handleHome} w={"50%"} borderRadius={8} bg="orange.400" _text={{ bold: true, color: "white", fontSize:"lg", textAlign: "center"}}>Voltar ao início</Button>
+        </HStack>
     </VStack>
     </ScrollView>
     );
